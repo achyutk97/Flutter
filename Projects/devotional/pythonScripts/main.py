7912737482318fd203932c9c72e574cb446a3c54
Achyut Kulkarni <achyutkulkarni43@gmail.com>
2024-09-15 22:32:53 +0530

    TASK: Modifying existing data added

--- Projects/devotional/pythonScripts/main.py
+++ Projects/devotional/pythonScripts/main.py
@@ -27,6 +27,7 @@ def MyPage(page1: ft.Page):
         if dd.value == "Add new Record":
             def submitClicked(e):
                 def submitCategpories(e):
+                    
                     global dd
                     sub = {
                         "name": name1.value,
@@ -81,6 +82,7 @@ def MyPage(page1: ft.Page):
             page.controls.extend([name, image, yesNo, submitButton])
             page1.update()
         else:
+
             def submitClicked(e):
                 def submitCategpories(e):
                     # print(name1.value)
@@ -122,6 +124,78 @@ def MyPage(page1: ft.Page):
                     submitButton = ft.ElevatedButton(text="Submit", on_click=submitCategpories)
                     page.controls.extend([name1, author, description, submitButton])
                     page1.update()
+                elif yesNo.value == "Modify":
+                    def submitClicked1(e):
+                        # global myDict
+                        print(yesNo1.value)
+                        def submitCategpories1(e):
+                            # global myDict
+                            print(name1.value)
+                            print(author.value)
+                            print(description.value)
+                            print(mainIndex.value)
+                            print(idx.value)
+                            index = mainIndex.value.split(".")
+                            myDict = readingTheJsonData()
+                            sub = {
+                                "name": name1.value,
+                                "author": author.value,
+                                "description":description.value
+                            }
+
+                            data = myDict['mainCategories'][int(index[0])-1][index[1]]['subCategories'][int(idx.value)] = sub
+                            saveToJsonFile(myDict)
+                            page1.controls.clear()
+                            MyPage(page1)
+                            pass
+
+                        data = yesNo1.value.split(".")
+                        existingDict = myDict['mainCategories'][int(index[0])-1][index[1]]['subCategories'][int(data[0])]
+                        name1 = ft.TextField(label="Enter the Stotram", multiline=True)
+                        idx = ft.TextField(label="", multiline=True)
+                        idx.value = data[0]
+                        mainIndex = ft.TextField(label="", multiline=True)
+                        mainIndex.value = index[0] +"." + index[1]
+                        name1.value = existingDict['name']
+                        author = ft.TextField(
+                            label="Enter Author Name",
+                            multiline=True,
+                            min_lines=1,
+                            max_lines=3,
+                        )
+                        author.value = existingDict['author']
+                        description = ft.TextField(
+                            label="Enter Description",
+                            multiline=True,
+                            min_lines=1,
+                            max_lines=5,
+                        )
+
+                        description.value = existingDict['description']
+                        submitButton = ft.ElevatedButton(text="Submit", on_click=submitCategpories1)
+                        ss = ft.Column(
+                            # scroll=ft.ScrollMode.ALWAYS,
+                            expand=True, # first alternative
+                            # height=page.height, # second alternative (without expand=True)
+                            controls=[idx, mainIndex, name1, author, description, submitButton]
+                        ),
+                        page.controls.extend(ss)
+                        page1.update()
+
+                    # import pdb;pdb.set_trace()
+                    listMy = []
+                    for i in range(len(myDict['mainCategories'][int(index[0])-1][index[1]]['subCategories'])):
+                        listMy.append(
+                            ft.dropdown.Option(f"{i}.{myDict['mainCategories'][int(index[0])-1][index[1]]['subCategories'][i]['name']}")
+                        )
+                    
+                    yesNo1 = ft.Dropdown(
+                        options=listMy
+                    )
+                    submitButton = ft.ElevatedButton(text="Submit", on_click=submitClicked1)
+                    page.controls.extend([yesNo1, submitButton])
+                    
+                    page1.update()
                 else:
                     myDict['mainCategories'][int(index[0]) - 1][index[1]].update(dictMy)
                     saveToJsonFile(myDict)
@@ -134,7 +208,7 @@ def MyPage(page1: ft.Page):
             name.value = myDict['mainCategories'][int(index[0]) - 1][index[1]]['name']
             image = ft.TextField(label="Image Link", multiline=True)
             image.value = myDict['mainCategories'][int(index[0]) - 1][index[1]]['imgLink']
-            listOfData = [ft.dropdown.Option("Yes"), ft.dropdown.Option("No")]
+            listOfData = [ft.dropdown.Option("Yes"), ft.dropdown.Option("Modify"), ft.dropdown.Option("No")]
             yesNo = ft.Dropdown(
                 width=100,
                 options=listOfData
@@ -161,11 +235,13 @@ def MyPage(page1: ft.Page):
     )
     # print(GLOBAL_ID_COUNT)
     page = ft.Column(
-        scroll= True,
+        scroll=ft.ScrollMode.ALWAYS,
+         expand=True,
         controls=[
             dd, b, t
         ]
     )
+    page1.auto_scroll = True
     page1.add(page)
 
 def main(Page):
